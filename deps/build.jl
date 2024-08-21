7d9bc16227420205d8397395413568a5393cfb94
Andrey Zakharevich <andreyzakharevich@gmail.com>
2024-08-21 12:58:21 +0300

    Add cargo update to build step

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,8 @@
 using StitchBindings
 using Scratch
 build_folder = get_scratch!(StitchBindings, "build")
+cmd = Cmd(`cargo update`, dir = "../stitch_bindings")
+run(cmd)
 cmd = Cmd(`cargo build -r --target-dir=$build_folder`, dir = "../stitch_bindings")
 run(cmd)
 bin_folder = get_scratch!(StitchBindings, "bin")
